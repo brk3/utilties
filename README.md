daed873abd66b48723e6741ddb238bca3ddea219
Paul Bourke <paul.bourke@oracle.com>
2018-09-13 14:05:20 +0100

    docker-proxy: restart daemon

---
+++ README.md
@@ -0,0 +1,7 @@
+# Utilities
+
+## docker-proxy
+```bash
+curl -H 'Cache-Control: no-cache' \
+       https://raw.githubusercontent.com/brk3/utilties/master/docker-proxy.sh?$(date +%s) | bash -x
+```
--- docker-proxy.sh
+++ docker-proxy.sh
@@ -3,5 +3,8 @@
 mkdir -p /etc/systemd/system/docker.service.d
 cat << EOF > /etc/systemd/system/docker.service.d/http-proxy.conf
 [Service]
-Environment="HTTPS_PROXY=http://proxy.odl.local:3128"
+Environment="HTTP_PROXY=http://proxy.odl.local:3128" "HTTPS_PROXY=http://proxy.odl.local:3128" "NO_PROXY=localhost,127.0.0.0,my-registry:5000"
 EOF
+
+systemctl daemon-reload
+systemctl restart docker
