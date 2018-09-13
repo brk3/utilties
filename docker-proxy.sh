0a23c902ac1ff962a1452e60df36f9e5dff8bf7f
Paul Bourke <brk3@users.noreply.github.com>
2018-09-13 14:28:24 +0100

    Update docker-proxy.sh

--- docker-proxy.sh
+++ docker-proxy.sh
@@ -3,7 +3,7 @@
 mkdir -p /etc/systemd/system/docker.service.d
 cat << EOF > /etc/systemd/system/docker.service.d/http-proxy.conf
 [Service]
-Environment="HTTP_PROXY=http://proxy.odl.local:3128" "HTTPS_PROXY=http://proxy.odl.local:3128" "NO_PROXY=localhost,127.0.0.0,my-registry:5000"
+Environment="HTTP_PROXY=http://proxy.odl.local:3128" "HTTPS_PROXY=http://proxy.odl.local:3128" "NO_PROXY=localhost,127.0.0.0,myregistry:5000"
 EOF
 
 systemctl daemon-reload
